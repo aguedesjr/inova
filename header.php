63feebaae2a95df4a5d413d2c00bc1fdef6c25e3
agsoares <agsoares@gmail.com>
2015-06-12 23:25:25 -0300

    inicio da construção do site

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8"> <!-- CODIFICACAO -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<title>Locações Inova</title> <!-- TITULO DA PAGINA -->
+	
+	<link href="css/bootstrap.css" rel="stylesheet"> <!-- Bootstrap Core CSS -->
+    <link href="css/modern-business.css" rel="stylesheet"> <!-- Custom CSS -->
+    <link href="css/font-awesome.css" rel="stylesheet" type="text/css"> <!-- Custom Fonts -->
+    <link href="css/styles.css" rel="stylesheet" type="text/css"> <!-- Menu -->
+	    
+</head>
